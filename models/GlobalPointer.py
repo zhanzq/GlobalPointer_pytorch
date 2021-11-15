1f6cdaf470b6203eba68c34ca70a14bda4bd54a6
zhanzq <zhanzhiqiang09@126.com>
2021-11-15 16:10:37 +0800

    format code

--- models/GlobalPointer.py
+++ models/GlobalPointer.py
@@ -1,18 +1,19 @@
-"""
-Date: 2021-06-02 00:33:09
-LastEditors: GodK
-"""
-import sys
+# !/usr/bin/env python
+# encoding=utf-8
+# author: zhanzq
+# email : zhanzhiqiang09@126.com
+# date  : 2021/11/15
+#
 
-sys.path.append("../")
-from common.utils import Preprocessor
 import torch
-import numpy as np
-from torch.utils.data import DataLoader, Dataset
 import torch.nn as nn
+from torch.utils.data import Dataset
+
+import numpy as np
+from common.utils import Preprocessor
 
 
-class MyDataset(Dataset):
+class XPNER(Dataset):
     def __init__(self, data):
         self.data = data
         self.length = len(data)
@@ -25,17 +26,17 @@ class MyDataset(Dataset):
 
 
 class DataMaker(object):
-    def __init__(self, tokenizer, add_special_tokens = True):
+    def __init__(self, tokenizer, add_special_tokens=True):
         super().__init__()
         self.tokenizer = tokenizer
         self.add_special_tokens = add_special_tokens
         self.preprocessor = Preprocessor(tokenizer, self.add_special_tokens)
 
-    def generate_inputs(self, datas, max_seq_len, ent2id, data_type="train"):
+    def generate_inputs(self, data, max_seq_len, ent2id, data_type="train"):
         """生成喂入模型的数据
 
         Args:
-            datas (list): json格式的数据[{'text':'','entity_list':[(start,end,ent_type),()]}]
+            data (list): json格式的数据[{'text':'','entity_list':[(start,end,ent_type),()]}]
             max_seq_len (int): 句子最大token数量
             ent2id (dict): ent到id的映射
             data_type (str, optional): data类型. Defaults to "train".
@@ -47,7 +48,7 @@ class DataMaker(object):
         ent_type_size = len(ent2id)  # 实体类别
 
         all_inputs = []
-        for sample in datas:
+        for sample in data:
             inputs = self.tokenizer(
                 sample["text"],
                 max_length=max_seq_len,
@@ -65,18 +66,19 @@ class DataMaker(object):
                     labels[ent2id[label], start, end] = 1
             inputs["labels"] = labels
 
-            input_ids = torch.tensor(inputs["input_ids"]).long()
-            attention_mask = torch.tensor(inputs["attention_mask"]).long()
-            token_type_ids = torch.tensor(inputs["token_type_ids"]).long()
+            input_ids = torch.tensor(inputs["input_ids"])
+            attention_mask = torch.tensor(inputs["attention_mask"])
+            token_type_ids = torch.tensor(inputs["token_type_ids"])
             if labels is not None:
-                labels = torch.tensor(inputs["labels"]).long()
+                labels = torch.tensor(inputs["labels"])
 
             sample_input = (sample, input_ids, attention_mask, token_type_ids, labels)
 
             all_inputs.append(sample_input)
         return all_inputs
-    
-    def generate_batch(self, batch_data, data_type="train"):
+
+    @staticmethod
+    def generate_batch(batch_data, data_type="train"):
         sample_list = []
         input_ids_list = []
         attention_mask_list = []
@@ -94,49 +96,59 @@ class DataMaker(object):
         batch_input_ids = torch.stack(input_ids_list, dim=0)
         batch_attention_mask = torch.stack(attention_mask_list, dim=0)
         batch_token_type_ids = torch.stack(token_type_ids_list, dim=0)
-        batch_labels = torch.stack(labels_list, dim=0) if data_type!="test" else None
-        
-        return (sample_list, batch_input_ids, batch_attention_mask, batch_token_type_ids, batch_labels)
-    
+        batch_labels = torch.stack(labels_list, dim=0) if data_type != "test" else None
+
+        batch_output = {
+            "sample_list": sample_list,
+            "batch_input_ids": batch_input_ids,
+            "batch_attention_mask": batch_attention_mask,
+            "batch_token_type_ids": batch_token_type_ids,
+            "batch_labels": batch_labels
+        }
+        return batch_output
+
     def decode_ent(self, pred_matrix):
         pass
+
 
 class MetricsCalculator(object):
     def __init__(self):
         super().__init__()
-    
-    def get_sample_f1(self, y_pred, y_true):
+
+    @staticmethod
+    def get_sample_f1(y_pred, y_true):
         y_pred = torch.gt(y_pred, 0).float()
-        return 2 * torch.sum(y_true * y_pred) / torch.sum(y_true + y_pred)
-    
-    def get_sample_precision(self, y_pred, y_true):
+        return 2 * torch.sum(y_true * y_pred) / torch.sum(y_true + y_pred + 1e-5)
+
+    @staticmethod
+    def get_sample_precision(y_pred, y_true):
         y_pred = torch.gt(y_pred, 0).float()
         return torch.sum(y_pred[y_true == 1]) / (y_pred.sum()+1)
-    
-    def get_evaluate_fpr(self, y_pred, y_true):
+
+    @staticmethod
+    def get_evaluate_fpr(y_pred, y_true):
         y_pred = y_pred.cpu().numpy()
         y_true = y_true.cpu().numpy()
         pred = []
         true = []
-        for b, l, start, end in zip(*np.where(y_pred>0)):
+        for b, l, start, end in zip(*np.where(y_pred > 0)):
             pred.append((b, l, start, end))
-        for b, l, start, end in zip(*np.where(y_true>0)):
+        for b, l, start, end in zip(*np.where(y_true > 0)):
             true.append((b, l, start, end))
 
         R = set(pred)
         T = set(true)
-        X = len(R & T)
-        Y = len(R)
-        Z = len(T)
+        X = len(R & T) + 1e-5
+        Y = len(R) + 1e-5
+        Z = len(T) + 1e-5
         f1, precision, recall = 2 * X / (Y + Z), X / Y, X / Z
         return f1, precision, recall
-    
-    
 
 
 class GlobalPointer(nn.Module):
-    def __init__(self, encoder, ent_type_size, inner_dim, RoPE=True):
+    def __init__(self, encoder, ent_type_size, inner_dim, RoPE=False):
         super().__init__()
+        self.device = None
         self.encoder = encoder
         self.ent_type_size = ent_type_size
         self.inner_dim = inner_dim
@@ -144,7 +156,6 @@ class GlobalPointer(nn.Module):
         self.dense = nn.Linear(self.hidden_size, self.ent_type_size * self.inner_dim * 2)
 
         self.RoPE = RoPE
-    
     
     def sinusoidal_position_embedding(self, batch_size, seq_len, output_dim):
         position_ids = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(-1)
@@ -160,32 +171,29 @@ class GlobalPointer(nn.Module):
         
     def forward(self, input_ids, attention_mask, token_type_ids):
         self.device = input_ids.device
-        
         context_outputs = self.encoder(input_ids, attention_mask, token_type_ids)
-        # last_hidden_state:(batch_size, seq_len, hidden_size)
-        last_hidden_state = context_outputs[0]
+        last_hidden_state = context_outputs[0]  # shape (batch_size, seq_len, hidden_size)
 
         batch_size = last_hidden_state.size()[0]
         seq_len = last_hidden_state.size()[1]
 
-        # outputs:(batch_size, seq_len, ent_type_size*inner_dim*2)
-        outputs = self.dense(last_hidden_state)
+        outputs = self.dense(last_hidden_state)  # shape (batch_size, seq_len, ent_type_size*inner_dim*2)
         outputs = torch.split(outputs, self.inner_dim * 2, dim=-1)
-        # outputs:(batch_size, seq_len, ent_type_size, inner_dim*2)
-        outputs = torch.stack(outputs, dim=-2)
+        outputs = torch.stack(outputs, dim=-2)  # shape (batch_size, seq_len, ent_type_size, inner_dim*2)
+
         # qw,kw:(batch_size, seq_len, ent_type_size, inner_dim)
-        qw, kw = outputs[...,:self.inner_dim], outputs[...,self.inner_dim:] # TODO:修改为Linear获取？
+        qw, kw = outputs[..., :self.inner_dim], outputs[..., self.inner_dim:]    # TODO:修改为Linear获取？
 
         if self.RoPE:
             # pos_emb:(batch_size, seq_len, inner_dim)
             pos_emb = self.sinusoidal_position_embedding(batch_size, seq_len, self.inner_dim)
             # cos_pos,sin_pos: (batch_size, seq_len, 1, inner_dim)
             cos_pos = pos_emb[..., None, 1::2].repeat_interleave(2, dim=-1)
-            sin_pos = pos_emb[..., None,::2].repeat_interleave(2, dim=-1)
-            qw2 = torch.stack([-qw[..., 1::2], qw[...,::2]], -1)
+            sin_pos = pos_emb[..., None, ::2].repeat_interleave(2, dim=-1)
+            qw2 = torch.stack([-qw[..., 1::2], qw[..., ::2]], -1)
             qw2 = qw2.reshape(qw.shape)
             qw = qw * cos_pos + qw2 * sin_pos
-            kw2 = torch.stack([-kw[..., 1::2], kw[...,::2]], -1)
+            kw2 = torch.stack([-kw[..., 1::2], kw[..., ::2]], -1)
             kw2 = kw2.reshape(kw.shape)
             kw = kw * cos_pos + kw2 * sin_pos
             
@@ -194,15 +202,10 @@ class GlobalPointer(nn.Module):
 
         # padding mask
         pad_mask = attention_mask.unsqueeze(1).unsqueeze(1).expand(batch_size, self.ent_type_size, seq_len, seq_len)
-        # pad_mask_h = attention_mask.unsqueeze(1).unsqueeze(-1).expand(batch_size, self.ent_type_size, seq_len, seq_len)
-        # pad_mask = pad_mask_v&pad_mask_h
         logits = logits*pad_mask - (1-pad_mask)*1e12
 
         # 排除下三角
         mask = torch.tril(torch.ones_like(logits), -1) 
         logits = logits - mask * 1e12
         
-
-        
         return logits/self.inner_dim**0.5
-
